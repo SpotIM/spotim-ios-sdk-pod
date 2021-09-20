e414d6d3431b41cfc42370197f9ede21eccc1a25
Alon Shprung <alon@cherrypick-consulting.com>
2021-09-20 10:43:35 +0300

    fix

--- Package.swift
+++ Package.swift
@@ -32,6 +32,7 @@ let package = Package(
         .target(
             name: "SpotIMCore",
             dependencies: ["SpotIMCoreXCFramework", "Alamofire", "PromiseKit"],
+            path: "SpotIMCore",
             exclude: ["SpotImAdsProvider"]
         )
     ]
