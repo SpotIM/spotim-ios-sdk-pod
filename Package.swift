1a19b2dca9ce153c765dcca2926f433d5a18392a
Refael Sommer <115537650+refaelOW@users.noreply.github.com>
2023-06-25 13:13:01 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -24,7 +24,11 @@ let package = Package(
         ),
         .target(
             name: "SpotIMCore",
-            dependencies: [.target(name: "SpotImCore", condition: .when(platforms: .some([.iOS]))), "RxSwift", "RxCocoa"]
+            dependencies: [
+                .target(name: "SpotImCore", condition: .when(platforms: .some([.iOS]))),
+                "RxSwift",
+                .product(name: "RxCocoa", package: "RxSwift")
+            ]
         )
     ]
 )
