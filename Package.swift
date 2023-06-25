4230b8db9ec2599968b05c6d4e5f5c4cbebad0e3
Refael Sommer <115537650+refaelOW@users.noreply.github.com>
2023-06-25 14:46:06 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
             name: "WrapperSPMTarget",
             dependencies: [
                 .target(name: "SpotImCore", condition: .when(platforms: .some([.iOS]))),
-                .target(name: "RxSwift", condition: .when(platforms: .some([.iOS])))
+                "RxSwift"
             ]
         )
     ]
