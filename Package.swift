8727fbe1e62d553ee8be7d39674a41acecfbbe8a
Refael Sommer <115537650+refaelOW@users.noreply.github.com>
2023-06-25 11:37:30 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
             path: "SpotImCore.xcframework"
         ),
         .target(
-            name: "SpotImCore",
+            name: "WrapperSPMTarget",
             dependencies: ["RxSwift", "RxCocoa"])
     ]
 )
