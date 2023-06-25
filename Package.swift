421b0234a60a30f3df0bb6df97228330d9c88672
Refael Sommer <115537650+refaelOW@users.noreply.github.com>
2023-06-25 13:27:30 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .iOS(.v11)
     ],
     products: [
-        .library(name: "SpotIMCore", targets: ["SpotIMCore"])
+        .library(name: "SpotIMCore", targets: ["spm"])
     ],
     dependencies: [
         // Here we define our package's external dependencies
@@ -23,7 +23,7 @@ let package = Package(
             path: "SpotImCore.xcframework"
         ),
         .target(
-            name: "SpotIMCore",
+            name: "spm",
             dependencies: [
                 .target(name: "SpotImCore", condition: .when(platforms: .some([.iOS]))),
                 "RxSwift",
