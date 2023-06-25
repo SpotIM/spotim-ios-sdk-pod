1df2cca9ae1bec8f1847a21073fe8134dba4ede4
Refael Sommer <115537650+refaelOW@users.noreply.github.com>
2023-06-26 00:53:13 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,10 +12,6 @@ let package = Package(
     dependencies: [
         // Here we define our package's external dependencies
         // and from where they can be fetched:
-        .package(
-            url: "https://github.com/ReactiveX/RxSwift.git",
-            .upToNextMinor(from: "6.5.0")
-        )
     ],
     targets: [
         .binaryTarget(
